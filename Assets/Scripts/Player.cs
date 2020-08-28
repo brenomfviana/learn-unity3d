c5804b65b4ce059aa47a01b11e15e44a2a1a0370
Breno Viana <bmfviana@gmail.com>
2020-08-27 22:33:28 -0300

    adds initial project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  //
+  public float moveSpeed = 5f;
+
+  //
+  bool jump = false;
+
+  void Update() {
+    //
+    Jump();
+    // Get player's horizontal move
+    Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+    transform.position += movement * Time.deltaTime * moveSpeed;
+  }
+
+  void Jump() {
+    if (Input.GetButtonDown("Jump")) {
+      GameObject sprite = gameObject.transform.GetChild(0).gameObject;
+      Rigidbody2D body = sprite.GetComponent<Rigidbody2D>();
+      body.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+    }
+  }
+}
