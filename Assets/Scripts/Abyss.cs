30dade361ff4786bd24b1cfc533eed7d337c6959
Breno Viana <bmfviana@gmail.com>
2020-09-01 16:08:04 -0300

    updates level 1, creates level 2 and follower camera

--- Assets/Scripts/Abyss.cs
+++ Assets/Scripts/Abyss.cs
@@ -5,7 +5,7 @@ public class Abyss : MonoBehaviour {
   private void OnTriggerEnter2D(Collider2D other) {
     if (other.gameObject.CompareTag("Player")) {
       // Destroy(other.gameObject);
-      SceneManager.LoadScene("Level1");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
   }
 }
