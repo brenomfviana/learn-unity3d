82c32b2fb078948df5c30f12758453d3f8f2e077
Breno Viana <bmfviana@gmail.com>
2020-09-06 14:38:20 -0300

    implements instructions button in main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,10 +13,6 @@ public class MainMenu : MonoBehaviour {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
   }
 
-  public void Instructions() {
-    // TODO
-  }
-
   public void ExitGame() {
     Application.Quit();
   }
