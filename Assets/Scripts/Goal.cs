9e6d011372f5bdf36ead68d166a8eccdadedcb78
Breno Viana <bmfviana@gmail.com>
2020-09-06 14:17:59 -0300

    implements main menu functions

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour {
   private void OnTriggerEnter2D(Collider2D other) {
     if (other.gameObject.CompareTag("Player")) {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
   }
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+  public void NewGame() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  public void LoadGame() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  public void Instructions() {
+    // TODO
+  }
+
+  public void ExitGame() {
+    Application.Quit();
+  }
+}
