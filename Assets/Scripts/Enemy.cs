2e4c3f3849037a2a00d9c8733f769579d1e390d4
Breno Viana <bmfviana@gmail.com>
2020-09-22 14:52:49 -0300

    implements an enemy and adds a new level

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Abyss : MonoBehaviour {
+public class Enemy : MonoBehaviour {
   private void OnTriggerEnter2D(Collider2D other) {
     if (other.gameObject.CompareTag("Player")) {
       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
---
+++ Assets/Scripts/EnemyMace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMace : MonoBehaviour {
+
+  private static float ERROR = 0.01f;
+
+  public float startvpos, endvpos;
+  public bool falling;
+  public float velocity;
+
+  void FixedUpdate() {
+    Vector3 pos = gameObject.transform.position;
+    if (!falling && pos.y < endvpos) {
+      pos.y += velocity;
+    } else {
+      pos.y -= velocity;
+    }
+    //
+    if (pos.y < startvpos + ERROR) {
+      falling = false;
+    } else if (pos.y > endvpos - ERROR) {
+      falling = true;
+    }
+    gameObject.transform.position = pos;
+  }
+}
